d0a61e4e0a3d79e80fff0cb67c5107b0d3d7f229
YAXpt <aylachic@gmail.com>
2023-11-17 10:35:07 +0100

    8th commit

--- HarryPotter.js
+++ HarryPotter.js
@@ -8,31 +8,26 @@ async function getWizards() {
     return data;
 }
 
-/*async function getWizards() {
-    const respuesta = await fetch(`${API_URL}/Wizards`);
-    const data = await respuesta.json();
-    console.table(data[0].elixirs);
-    console.log(data);
-    return data;
-}*/
 
 window.onload = async () => { //se esperará a q cargue el html y luego ejecutará este cacho de código
 
-      const dataHP = await getWizards();
-      const element = document.getElementById('personatges');
-      debugger;
-      
-      //debugger;
-      
-      for (const wizard of dataHP) {
-        const newElementLi = document.createElement('li');
-        newElementLi.innerHTML += "<h2>" + wizard.lastName + "</h2>";
-        element.appendChild(newElementLi);
-        for (const elix of dataHP[1].elixirs){
-            const newElementP = document.createElement('p');
-            newElementP.innerHTML += elix.name;
-            element.appendChild(newElementP);
-            }
-      }
+    const dataHP = await getWizards();
 
-    };
+    const element = document.getElementById('personatges');
+
+    //debugger;
+
+    for (const wizard of dataHP) {
+        const newElement = document.createElement('li');
+        newElement.innerHTML = `<h2> ${wizard.lastName} </h2>`;
+        for (const elixir of wizard.elixirs) {
+            newElement.innerHTML += `<button onclick="getIngredients()">Ingredients</button> ${elixir.name}`;
+        }
+        element.appendChild(newElement);
+      /*for(const elix of dataHP[1].elixirs){ //mcet
+        const newElementP = document.createElement('p');
+        newElementP.innerHTML += elix.name;
+        element.appendChild(newElementP);
+        }*/
+    }
+};
