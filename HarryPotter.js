50b5ca99f9d66a427926a7a5a9ba62ff5370acf3
YAXpt <aylachic@gmail.com>
2023-11-17 11:29:45 +0100

    Ultim commit de classe

--- HarryPotter.js
+++ HarryPotter.js
@@ -9,17 +9,16 @@ async function getWizards() {
 }
 
 async function mostrarIngredients(idElixir){
-    var ubicacion = document.getElementById('personatges');
+    var ubicacion = document.getElementById(idElixir);
     const respuesta = await fetch(`${API_URL}/Elixirs/${idElixir}`);
     const data = await respuesta.json();
     console.table(data);
     console.log(data);
-    debugger;
+    //debugger;
     for (const ingredient of data.ingredients){
         const newElement = document.createElement('li');
-        newElement.textContent = ingredient.name;
-        ubicacion.appendChild(newElement)
-        //ubicacion.innerHTML += `<li>${ingredient.name}</li>`;     
+        newElement.innerHTML += ingredient.name; 
+        ubicacion.appendChild(newElement);    
     }
     
     /*ubicacion.innerHTML += `
@@ -44,7 +43,7 @@ window.onload = async () => { //se esperará a q cargue el html y luego ejecutar
         const newElement = document.createElement('ul');
         newElement.innerHTML = `<li><h2> ${wizard.lastName} </h2><li>`;
         for (const elixir of wizard.elixirs) {
-            newElement.innerHTML += `<br>${elixir.name}</br><button onclick="mostrarIngredients('${elixir.id}')">Ingredients</button>`;
+            newElement.innerHTML += `<br>${elixir.name}</br><button id="${elixir.id}" onclick="mostrarIngredients('${elixir.id}')">Ingredients</button>`;
         }
         element.appendChild(newElement);
       /*for(const elix of dataHP[1].elixirs){ //mcet
