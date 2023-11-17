b024e511a7a74593a5df1b1491be9defe2199cf6
YAXpt <aylachic@gmail.com>
2023-11-17 09:15:49 +0100

    3r commit

--- HarryPotter.js
+++ HarryPotter.js
@@ -2,21 +2,34 @@ const API_URL = 'https://wizard-world-api.herokuapp.com';
 
 async function getWizards() {
     const respuesta = await fetch(`${API_URL}/Wizards`);
-    debugger;
     const data = await respuesta.json();
-    console.table(data.elixirs);
+    console.table(data);
     console.log(data);
     return data;
 }
+
+/*async function getWizards() {
+    const respuesta = await fetch(`${API_URL}/Wizards`);
+    const data = await respuesta.json();
+    console.table(data[0].elixirs);
+    console.log(data);
+    return data;
+}*/
 
 window.onload = async () => { //se esperará a q cargue el html y luego ejecutará este cacho de código
 
       const dataHP = await getWizards();
       const element = document.getElementById('personatges');
 
-      for (const anime of animes) {
+      for (const wizard of dataHP) {
         const newElement = document.createElement('li');
-        newElement.innerHTML += "<h2>" + dataHP.data + "</h2>";
+        newElement.innerHTML += "<h2>" + wizard.firstname + "</h2>";
         element.appendChild(newElement);
       }
+
+      /*for (var wiz=0; wiz<17; wiz++) {
+        const newElement = document.createElement('li');
+        newElement.innerHTML += dataHP[wiz].lastName;
+        element.appendChild(newElement);
+      }*/
   };
